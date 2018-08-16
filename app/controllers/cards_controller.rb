0c67179620d64bc4966caf6c8497fe6090eab69a
b-forster <brianna.forster@gmail.com>
2018-08-15 19:37:04 -0700

    Add action/view for adding new cards to a deck

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,4 +5,31 @@ class CardsController < ApplicationController
     @deck = Deck.find(params[:deck_id])
     @cards = @deck.cards
   end
+
+  def new
+    @deck = Deck.find(params[:deck_id])
+  end
+
+  def create
+    @deck = Deck.find(params[:deck_id])
+
+    @card = Card.new(card_params)
+    @card.deck = @deck
+
+    if @card.save
+      flash[:notice] = ["Card added successfully"]
+
+      redirect_to deck_cards_path
+    else
+      flash[:alert] = @card.errors.full_messages
+
+      render :new
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:cards).permit(:front, :back, :due, :deck)
+  end
 end
