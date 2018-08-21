ec025e2e9f6cfe61e6a219b35b2a74879756525f
b-forster <brianna.forster@gmail.com>
2018-08-20 23:05:03 -0700

    DRY up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   get 'errors/internal_server_error'
   root to: 'decks#index' 
     
-    # sign up page with form:
-  get 'users/new' => 'users#new', as: :new_user
+  # sign up page with form:
+  get 'signup' => 'users#new', as: :new_user
   
   # create (post) action for when sign up form is submitted:
   post 'users' => 'users#create'
-
-  resources :decks do
-    resources :cards
-  end
 
   # log in page with form:
   get '/login'     => 'sessions#new'
@@ -21,6 +17,10 @@ Rails.application.routes.draw do
   
   # delete action to log out:
   delete '/logout' => 'sessions#destroy'
+
+  resources :decks do
+    resources :cards, dependent: :destroy
+  end
 
   match "/404", :to => "errors#not_found", :via => :all
   match "/500", :to => "errors#internal_server_error", :via => :all
