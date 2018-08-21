e6d01bf0b9686c606ac2186fce17774009651821
b-forster <brianna.forster@gmail.com>
2018-08-20 19:01:20 -0700

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Card.delete_all
+Deck.delete_all
+User.delete_all
+
+
+3.times {
+  User.create(
+    name: Faker::SiliconValley.unique.character,
+    email: Faker::SiliconValley.unique.email,
+    password: 'test'
+  )
+}
+
+User.all.each { |user| Deck.create(
+    name: Faker::SiliconValley.invention,
+    description: Faker::SiliconValley.unique.motto,
+    user: user
+  )
+}
+
+Deck.all.each { |deck| 5.times {
+  Card.create(
+    front: Faker::SiliconValley.quote,
+    back: Faker::SiliconValley.app,
+    deck: deck
+  )
+}
+
+}
