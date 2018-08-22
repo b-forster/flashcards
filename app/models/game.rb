baed8deafa873e54900a77b435101bda01ba7160
b-forster <brianna.forster@gmail.com>
2018-08-21 17:26:50 -0700

    Set cards_count attribute when a new Game is created

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,10 @@ class Game < ApplicationRecord
   belongs_to :deck
   has_many :cards, class_name: 'GameCard'
   has_one :user, through: :deck
+
+  before_save :set_cards_count
+
+  def set_cards_count
+    self.cards_count = self.cards.size
+  end
 end
