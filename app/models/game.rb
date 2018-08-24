3b2b76c1196b69f1056b37a41c5c0eb8e268b735
b-forster <brianna.forster@gmail.com>
2018-08-23 22:28:39 -0700

    [Debug] save Game after updating initial_cards_count

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ApplicationRecord
   has_many :cards, class_name: 'GameCard', dependent: :destroy
   has_one :user, through: :deck
 
-  after_save :generate_and_count_game_cards
+  after_create :generate_and_count_game_cards
 
   def generate_and_count_game_cards
     due_cards = self.deck.cards.where(due: true)
@@ -14,7 +14,6 @@ class Game < ApplicationRecord
   end
 
   def generate_game_cards(game_cards)
-
     game_cards.each do |card|
       GameCard.create(game: self, card: card)
     end
@@ -22,10 +21,7 @@ class Game < ApplicationRecord
 
   def set_initial_cards_count(num)
     self.initial_cards_count = num
-    p "%" * 100
-    p "self is " + self.to_s
-    p "initial_cards_count is " + self.initial_cards_count.to_s
-    p "%" * 100
+    self.save
   end
 
   def remaining_cards_count
