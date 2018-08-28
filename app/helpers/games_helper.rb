da880df854d8226bb3566f44c0ea92cbf4f724a7
b-forster <brianna.forster@gmail.com>
2018-08-28 00:36:26 -0700

    Show game completed message when all cards in a deck have been reviewed with a correct answer

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,21 +1,26 @@
 module GamesHelper
   def render_card(game_id)
     @game = Game.find(game_id)
-    @remaining_cards = @game.cards.shuffle
-    @deck = @game.deck
 
-    next_card = @remaining_cards.last
-
-    until next_card do
-      next_card = @remaining_cards.last
-    end
-
-    @return_card = Card.find(next_card.card_id)
-
-    if request.xhr?
-      render partial: 'cards/review', layout: false, locals: { card: @return_card }
+    if @game.complete?
+      render partial: 'cards/complete'
     else
-      render 'games/show', locals: { card: @return_card }
+      @remaining_cards = @game.cards.shuffle
+      @deck = @game.deck
+
+      next_card = @remaining_cards.last
+
+      until next_card do
+        next_card = @remaining_cards.last
+      end
+
+      @return_card = Card.find(next_card.card_id)
+
+      if request.xhr?
+        render partial: 'cards/review', layout: false, locals: { card: @return_card }
+      else
+        render 'games/show', locals: { card: @return_card }
+      end
     end
   end
 end
